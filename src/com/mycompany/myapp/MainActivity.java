6cdc812333888a7134c131d756daf8ff7eba073d
Tito Luyo Murata <tluyo@hotmail.com>
2014-03-13 22:48:34 -0500

    primer cambio prueba

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -3,15 +3,31 @@ package com.mycompany.myapp;
 import android.app.*;
 import android.os.*;
 import android.view.*;
+import android.view.View.OnClickListener;
 import android.widget.*;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements OnClickListener
 {
+	Button btnSend;
+	EditText txtCorreo;
+	TextView txtMessage;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        btnSend = (Button)findViewById(R.id.txtCorreo);
+        btnSend.setOnClickListener(this);
+        txtCorreo = (EditText)findViewById(R.id.txtCorreo);
+        txtMessage = (TextView)findViewById(R.id.txtMessage);
     }
+
+	@Override
+	public void onClick(View v) {
+		txtMessage.setText(txtCorreo.getText());
+		
+	}
 }
